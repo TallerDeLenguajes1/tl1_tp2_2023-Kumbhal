16bc44cc9729a00ef70c004a3f43aa5da481190d
Cordoba Leandro <cordobaleandro000@gmail.com>
2023-04-11 00:40:16 -0300

    Ejercicio 4 completado

--- tp2_4.c
+++ tp2_4.c
@@ -12,11 +12,14 @@ struct{
     char *tipo_cpu;
 }typedef computadora;
 
+void mostrarOpciones();
 void cargarComputadoras(computadora *computadora, int cantidad, char tipos[C][L]);
 void mostrarComputadoras(computadora *computadora, int cantidad);
+void mostrarComputadoraVieja(computadora *computadora, int cantidad);
+void mostrarComputadoraRapida(computadora *computadora, int cantidad);
 
 void main(){
-    int cantidad = 0;
+    int cantidad = 0, opcion = 0;
     char tipos[C][L]= {"Intel","AMD","Celeron","Athlon","Core","Pentium"};
     srand(time(NULL));
     printf("Ingrese la cantidad de computadoras a generar: ");
@@ -24,13 +27,27 @@ void main(){
     fflush(stdin);
     computadora *computadora = malloc(cantidad*sizeof(computadora));
     cargarComputadoras(computadora, cantidad, tipos);
-    mostrarComputadoras(computadora, cantidad);
+    while (opcion != 4){
+        mostrarOpciones();
+        scanf("%d", &opcion);
+        fflush(stdin);
+        if (opcion == 1){
+            mostrarComputadoras(computadora, cantidad);
+        }else if(opcion == 2){
+            mostrarComputadoraVieja(computadora, cantidad);
+        }else{
+            mostrarComputadoraRapida(computadora, cantidad);
+        }
+    }
     free(computadora);
 }
 
+void mostrarOpciones(){
+    printf("\n----------OPCIONES----------\n(1)Mostrar Computadoras\n(2)Mostrar computadora mas vieja\n(3)Mostrar computadora mas rapida\n(4)Salir");
+}
 void cargarComputadoras(computadora *computadora, int cantidad, char tipos[C][L]){
     for (int i = 0; i < cantidad; i++){
-        (computadora+i)->velocidad = rand()%4;
+        (computadora+i)->velocidad = 1 + rand()%3;
         (computadora+i)->anio = 2015 + rand()%8;
         (computadora+i)->cantidad = 1 + rand()%9;
         (computadora+i)->tipo_cpu = tipos[rand()%C];
@@ -44,4 +61,32 @@ void mostrarComputadoras(computadora *computadora, int cantidad){
         printf("\nPosee %d nucleos", (computadora+i)->cantidad);
         printf("\nLa computadora es del anio %d", (computadora+i)->anio);
     }
+}
+void mostrarComputadoraVieja(computadora *computadora, int cantidad){
+    int i = 0, numero = 0, anio = 0;
+    anio = (computadora+i)->anio;
+    for (i; i < cantidad; i++){
+        if (anio >= ((computadora+i)->anio)){
+            anio = (computadora+i)->anio;
+            numero = i;
+        }
+    }
+    printf("\nEl tipo de CPU de la computadora es %s", (computadora+numero)->tipo_cpu);
+    printf("\nLa velocidad de la computadora es de %d GhZ", (computadora+numero)->velocidad);
+    printf("\nPosee %d nucleos", (computadora+numero)->cantidad);
+    printf("\nLa computadora es del anio %d", (computadora+numero)->anio);
+}
+void mostrarComputadoraRapida(computadora *computadora, int cantidad){
+    int i = 0, numero = 0, velocidad = 0;
+    velocidad = (computadora+i)->velocidad;
+    for (i; i < cantidad; i++){
+        if (velocidad <= ((computadora+i)->velocidad)){
+            velocidad = (computadora+i)->velocidad;
+            numero = i;
+        }
+    }
+    printf("\nEl tipo de CPU de la computadora es %s", (computadora+numero)->tipo_cpu);
+    printf("\nLa velocidad de la computadora es de %d GhZ", (computadora+numero)->velocidad);
+    printf("\nPosee %d nucleos", (computadora+numero)->cantidad);
+    printf("\nLa computadora es del anio %d", (computadora+numero)->anio);
 }
